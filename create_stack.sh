0b4b8f0ac6f1653dca727a5e0a0944e0e4427d48
ahmed hassan <50107057+ahmed-hassan97@users.noreply.github.com>
2022-12-31 21:45:03 +0200

    Update create_stack.sh

--- create_stack.sh
+++ create_stack.sh
@@ -1,4 +1,15 @@
+#!/bin/bash          
 ## pull container image from docker hup
-sudo docker container run -it --name "datastack_v2" -h hassan_v2 ahmed97hassan/big_data_tool:v2 bash
+sudo docker container run  -itd --name "datastack_v2" -h hassan_v2 ahmed97hassan/big_data_tool:v2 bash
+## get container id
+container_id=$(sudo docker ps -aqf "name=datastack_v2")
+## create directory
+mkdir project
+cd project
+## copy docker compose file from container to local
+sudo docker cp $container_id:/shared-volume/ .
+cd shared-volume
+## build docker image
 sudo docker-compose up -d
+
 
